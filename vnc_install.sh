479af4dc23dd33a956884b5ccb209b29f4d22834
Sean Dion <smdion@gmail.com>
2016-12-24 16:36:08 -0600

    Update vnc_install.sh

--- vnc_install.sh
+++ vnc_install.sh
@@ -6,15 +6,22 @@ echo "Please enter the VNC password followed by ENTER:"
 read mypasswd
 
 sudo apt-get -y install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal 
-sudo apt-get -y install vnc4server
+sudo apt-get -y install x11vnc
 mkdir /home/$myuser/.vnc
 echo $mypasswd | vncpasswd -f > /home/$myuser/.vnc/passwd
 chown -R $myuser:$myuser /home/$myuser/.vnc
 chmod 0600 /home/$myuser/.vnc/passwd
-vncserver :1
-echo "gnome-panel &" >> ~/.vnc/xstartup
-echo "gnome-settings-daemon &" >> ~/.vnc/xstartup
-echo "metacity &" >> ~/.vnc/xstartup
-echo "nautilus &" >> ~/.vnc/xstartup
-(crontab -l ; echo "@reboot /usr/bin/vncserver :1")| crontab -
-shutdown -r now
+x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbauth /home/$myuser/.vnc/passwd -rfbport 5900 -shared
+echo '
+description "x11vnc"
+
+start on runlevel [2345]
+stop on runlevel [^2345]
+
+console log
+
+respawn
+respawn limit 20 5
+
+exec /usr/bin/x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbauth /home/$myuser/.vnc/passwd -rfbport 5900 -shared' > /etc/init/x11vnc.conf
+
