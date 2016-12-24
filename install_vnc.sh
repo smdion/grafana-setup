561e343dd50d18d8b296b9d5f53e255a34b5f7b3
Sean Dion <smdion@gmail.com>
2016-12-24 15:36:18 -0600

    Create install_vnc.sh

---
+++ install_vnc.sh
@@ -0,0 +1,8 @@
+sudo apt-get install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
+sudo apt-get install vnc4server
+vncserver :1
+echo "gnome-panel &" >> ~/.vnc/xstartup
+echo "gnome-settings-daemon &" >> ~/.vnc/xstartup
+echo "metacity &" >> ~/.vnc/xstartup
+echo "nautilus &" >> ~/.vnc/xstartup
+(crontab -l ; echo "@reboot /usr/bin/vncserver :1")| crontab -
