9f6a507ab0af589fe7e7e7170b27651fc1ad13aa
Sean Dion <smdion@gmail.com>
2016-12-24 15:55:54 -0600

    Update install_vnc.sh

--- install_vnc.sh
+++ install_vnc.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+read -s -p "Enter Ubuntu Username" myuser
+read -s -p "Enter VNC Password" mypasswd
+
 sudo apt-get -y install ubuntu-desktop gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal 
 sudo apt-get -y install vnc4server
+mkdir /home/$myuser/.vnc
+echo $mypasswd | vncpasswd -f > /home/$myuser/.vnc/passwd
+chown -R $myuser:$myuser /home/$myuser/.vnc
+chmod 0600 /home/$myuser/.vnc/passwd
 vncserver :1
 echo "gnome-panel &" >> ~/.vnc/xstartup
 echo "gnome-settings-daemon &" >> ~/.vnc/xstartup
