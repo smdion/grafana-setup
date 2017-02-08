ebdb68379b9c3854e96b14c21fcbdc174c884309
Sean Dion <smdion@gmail.com>
2017-02-07 20:17:04 -0600

    Update fix_eth0.sh

--- fix_eth0.sh
+++ fix_eth0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' grub
+sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo reboot
